28c57ee9b80f905609a67ac024b1d4089bcf76e4
kitotake <ahmed12199725@gmail.com>
2026-06-03 22:10:09 +0200

    modif rotation et autre

--- client.lua
+++ client.lua
@@ -1,114 +1,136 @@
 local placedProp = nil
+
 local pitchValue = 0.0
-local pitchSpeed = 1.0 -- Vitesse de rotation du pitch
-local isRotating = true
+local pitchSpeed = 1.0
 
--- Fonction pour placer le prop
-function placeProp()
-    if not placedProp then
-        local playerPed = PlayerPedId()
-        local playerCoords = GetEntityCoords(playerPed)
-        local forward = GetEntityForwardVector(playerPed)
+local isRotating = false
+local canToggle = true
+local cooldownActive = false
 
-        -- Positionne le prop un peu devant le joueur
-        local propCoords = playerCoords + forward * 1.5
+-- ROTATION
+local function startRotation()
+    if isRotating then return end
+    if not placedProp then return end
 
-        -- Crée le prop avec les informations du fichier config.lua
-        local prop = CreateObject(GetHashKey(Config.PropName), propCoords.x, propCoords.y, propCoords.z, true, true, true)
-        SetEntityRotation(prop, Config.DefaultRotation.x, Config.DefaultRotation.y, Config.DefaultRotation.z, 2, true)
+    isRotating = true
 
-        -- Garde une référence du prop pour le manipuler
-        placedProp = prop
-        print("Prop placé.")
-    else
-        print("Un prop est déjà placé. Supprime-le d'abord.")
-    end
-end
+    print("^2[PROP]^0 Rotation ACTIVÉE ✔")
 
--- Fonction pour supprimer tous les props dans une zone
-function deletePropsInZone()
-    local playerPed = PlayerPedId()
-    local playerCoords = GetEntityCoords(playerPed)
+    CreateThread(function()
+        while isRotating and placedProp do
+            Wait(10)
 
-    local handle, object = FindFirstObject()
-    local success
+            pitchValue = pitchValue - pitchSpeed
 
-    repeat
-        local objCoords = GetEntityCoords(object)
-        local distance = #(playerCoords - objCoords)
-
-        -- Si l'objet est dans le rayon défini, on le supprime
-        if distance < Config.DeleteRadius then
-            if IsEntityAnObject(object) then
-                DeleteObject(object)
-                print("Prop supprimé.")
+            if pitchValue <= -100 then
+                pitchValue = -100
             end
+
+            local rot = GetEntityRotation(placedProp, 2)
+            SetEntityRotation(placedProp, rot.x, pitchValue, rot.z, 2, true)
         end
-
-        success, object = FindNextObject(handle)
-    until not success
-
-    EndFindObject(handle)
+    end)
 end
 
--- Fonction pour faire bouger le prop autour de son axe de pitch (Y)
-function rotatePropOnPitch()
+-- STOP
+local function stopRotation()
+    if not isRotating then return end
+
+    isRotating = false
+
+    print("^1[PROP]^0 Rotation STOP ✖")
+end
+
+-- COOLDOWN CLEAN
+local function startCooldown()
+    cooldownActive = true
+    canToggle = false
+
+    print("^3[PROP]^0 Cooldown 3000ms...")
+
+    CreateThread(function()
+        Wait(3000)
+
+        cooldownActive = false
+        canToggle = true
+
+        print("^2[PROP]^0 Cooldown terminé ✔")
+    end)
+end
+
+local function placeProp()
     if placedProp then
-        -- Ajuste la rotation pitch avec la vitesse définie
-        pitchValue = pitchValue + pitchSpeed
-        -- On récupère la rotation actuelle et on ajuste uniquement le pitch (Y)
-        local currentRotation = GetEntityRotation(placedProp, 2)
-        SetEntityRotation(placedProp, currentRotation.x, pitchValue, currentRotation.z, 2, true)
-        print("Pitch ajusté: " .. pitchValue)
-    else
-        print("Aucun prop n'est placé.")
+        print("^1[PROP]^0 Déjà un prop placé.")
+        return
     end
+
+    local ped = PlayerPedId()
+    local coords = GetEntityCoords(ped)
+    local forward = GetEntityForwardVector(ped)
+
+    local propCoords = coords + forward * 1.5
+
+    placedProp = CreateObject(
+        GetHashKey(Config.PropName),
+        propCoords.x, propCoords.y, propCoords.z,
+        true, true, true
+    )
+
+    SetEntityRotation(
+        placedProp,
+        Config.DefaultRotation.x,
+        Config.DefaultRotation.y,
+        Config.DefaultRotation.z,
+        2,
+        true
+    )
+
+    pitchValue = Config.DefaultRotation.y or 0.0
+
+    print("^2[PROP]^0 Prop placé ✔")
 end
 
--- Fonction pour détecter la touche E et ajuster le pitch
-Citizen.CreateThread(function()
+-- INPUT
+CreateThread(function()
     while true do
-        Citizen.Wait(0) -- Vérifie en permanence si la touche E est pressée
+        Wait(0)
 
-        -- Si le joueur appuie sur la touche E
-        if IsControlJustPressed(0, 38) then -- 38 est la touche 'E'
-            if placedProp then
-                -- Si le prop existe, démarre la rotation du pitch entre 1 et -100
-                isRotating = not isRotating -- Active ou désactive la rotation
+        if IsControlJustPressed(0, 38) then -- E
 
-                Citizen.CreateThread(function()
-                    while isRotating do
-                        Citizen.Wait(10) -- Intervalle de rotation rapide
-                        
-                        -- Bouger entre 1 et -100 degrés
-                        if pitchValue <= -500 then
-                            pitchSpeed = 1.0 -- Change la direction pour remonter
-                        elseif pitchValue >= 1 then
-                            pitchSpeed = -1.0 -- Change la direction pour descendre
-                        end
-
-                        -- Ajuste la rotation
-                        rotatePropOnPitch()
-                    end
-                end)
-            else
-                print("Aucun prop n'est placé.")
+            -- BLOCK INPUT (IMPORTANT)
+            if cooldownActive then
+                goto continue
             end
+
+            if not placedProp then
+                print("^1[PROP]^0 Aucun prop.")
+                goto continue
+            end
+
+            -- LOCK ANTI SPAM
+            startCooldown()
+
+            -- TOGGLE CLEAN
+            if isRotating then
+                stopRotation()
+            else
+                startRotation()
+            end
+
+            ::continue::
         end
     end
 end)
-
--- Commande pour placer le prop
-RegisterCommand('placeprop', function()
+-- PLACE COMMAND
+RegisterCommand("placeprop", function()
     placeProp()
-end, false)
+end)
 
--- Commande pour supprimer tous les props dans une zone définie
-RegisterCommand('deleteprop', function()
-    deletePropsInZone()
-end, false)
-
--- Commande pour faire bouger le prop sur le pitch manuellement
-RegisterCommand('pitchprop', function()
-    rotatePropOnPitch()
-end, false)
+-- DELETE (simple)
+RegisterCommand("deleteprop", function()
+    if placedProp then
+        DeleteObject(placedProp)
+        placedProp = nil
+        print("^1[PROP]^0 Prop supprimé.")
+    end
+end)
